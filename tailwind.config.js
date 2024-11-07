fa562582b55ff0eda878b8cb7e5f6f087a1cdb73
Victoria Joseph <josephv1791@gmail.com>
2024-11-06 21:44:40 -0500

    file structure

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ 
+    "./src/index.html**/*.{html,js}",
+    "./src/components/**/*.{html,js,}",
+    "./src/app/**/*.{html,js,}",],
+  theme: {
+    extend: {
+      darkMode: ['selector', '[data-mode="dark"]'],
+
+      colors: {
+        'bg-dark':'#03031e',
+        'text-color': '#f0fbda',
+        
+    },
+      fontFamily: {
+        'source-code-pro': ['"Source Code Pro"', 'monospace'],
+    },
+      container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    
+
+  ],
+}
+
+
+},
+
+
+}
+
