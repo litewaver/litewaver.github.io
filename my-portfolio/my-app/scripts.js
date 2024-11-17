4112edc074ff86d7a7d4f9cb6c3af44ca3a2b609
Victoria Joseph <josephv1791@gmail.com>
2024-11-16 20:54:33 -0500

    spotify

--- my-portfolio/my-app/scripts.js
+++ my-portfolio/my-app/scripts.js
@@ -6,3 +6,10 @@ document.body.innerHTML = '<div id="app"></div>';
 // Render your React component instead
 const root = createRoot(document.getElementById('app'));
 root.render(<h1>Hello, world</h1>);
+
+
+function MyComponent() {
+    return <h1>Hello from React!</h1>;
+  }
+  
+  ReactDOM.render(<MyComponent />, document.getElementById('react-root'));
