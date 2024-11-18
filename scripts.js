b7777024855fd94d9e9b1954a853f87f2023ecce
Victoria Joseph <josephv1791@gmail.com>
2024-11-18 13:52:29 -0500

    jquery

--- scripts.js
+++ scripts.js
@@ -17,3 +17,54 @@ function MyComponent() {
 */
 
 
+//back to top button
+
+// Get the button
+let mybutton = document.getElementById("topbtn");
+
+// Show the button when the user scrolls down 20px from the top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// Scroll to the top when the button is clicked
+mybutton.onclick = function() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+mybutton.onclick = function() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+
+
+//tilt effect for photo
+const cards = document.querySelectorAll('.tilt-card');
+
+cards.forEach(card => {
+  card.addEventListener('mousemove', e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = (y - centerY) / 10;
+    const rotateY = (centerX - x) / 10;
+
+    card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = 'perspective(1000px) rotateX(0) rotateY(0)';
+  });
+});
