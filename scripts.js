c8e74df44766b46895231e7ec5ccf734f62f3fbe
Victoria Joseph <josephv1791@gmail.com>
2024-11-17 21:14:14 -0500

    fix file

---
+++ scripts.js
@@ -0,0 +1,19 @@
+/* import { createRoot } from 'react-dom/client';
+
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React component instead
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>Hello, world</h1>);
+
+
+function MyComponent() {
+    return <h1>Hello from React!</h1>;
+  }
+  
+  ReactDOM.render(<MyComponent />, document.getElementById('react-root'));
+
+*/
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/index.html**/*.{html,js}",
+    "./src/components/**/*.{html,js,}",
+    "./src/app/**/*.{html,js,}",
+  ],
+  theme: {
+    extend: {
+
+      extend: {
+        keyframes: {
+          shine: {
+            '100%': { left: '125%' },
+          },
+        },
+        animation: {
+          shine: 'shine 1s',
+        },
+      },
+    },
+
+      backdropFilter: ["responsive"],
+      colors: {
+        "bg-dark": "#03031e",
+      },
+      fontFamily: {
+        "source-code-pro": ['"Source Code Pro"', "monospace"],
+      },
+      container: {
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+
+          width,
+        },
+      },
+      plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+      ],
+    },
+  },
+};
