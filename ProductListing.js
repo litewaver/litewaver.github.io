e94b6a94da552b425be6948b45fbd1dbe40fca3d
Victoria Joseph <josephv1791@gmail.com>
2025-06-19 18:28:28 -0400

    js

---
+++ ProductListing.js
@@ -0,0 +1,14 @@
+const products = {
+    name: "charm",
+    price: "$6",
+    description: "an exmaple of my physical works",
+    image: "assets/product1Image.jpg",
+    inStock: true,
+};
+
+const container = document.getElementById("product-container");
+
+products.forEach((product) => {
+  
+
+}
